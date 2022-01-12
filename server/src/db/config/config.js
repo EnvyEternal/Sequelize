eb7aa17e1ecfc7d8d7a3be7d2f160575d3c281ca
E Eternal <eeternal@Es-MacBook-Air.local>
2022-01-12 20:16:13 +0200

    initial state

--- server/src/db/config/config.js
+++ server/src/db/config/config.js
@@ -3,7 +3,7 @@ require('dotenv/config');
 module.exports = {
   development: {
     username: 'postgres',
-    password: '',
+    password: ' ',
     database: 'testdb',
     host: '127.0.0.1',
     port: 5432,
